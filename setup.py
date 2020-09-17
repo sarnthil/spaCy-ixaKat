a80662eb534265092ab48c75fc38c52ceefbf6b2
Koichi Yasuoka <yasuoka@kanji.zinbun.kyoto-u.ac.jp>
2020-09-17 18:47:55 +0900

    version 0.1.5

--- setup.py
+++ setup.py
@@ -1,5 +1,9 @@
 import setuptools,subprocess,platform
 
+with open("README.md","r",encoding="utf-8") as r:
+  long_description=r.read()
+URL="https://github.com/KoichiYasuoka/spaCy-ixaKit"
+
 if platform.system()=="Linux" and platform.machine()=="x86_64":
   subprocess.check_call(["./download.sh"])
 else:
@@ -8,8 +12,29 @@ packages=subprocess.check_output(["./packages.sh"]).decode("utf-8").rstrip().spl
 
 setuptools.setup(
   name="spacy_ixakat",
-  version="0.1.0",
+  version="0.1.5",
+  description="ixaKat wrapper for spaCy",
+  long_description=long_description,
+  long_description_content_type="text/markdown",
+  url=URL,
+  author="Koichi Yasuoka",
+  author_email="yasuoka@kanji.zinbun.kyoto-u.ac.jp",
+  license="MIT",
+  keywords="ixaKat spaCy",
   packages=setuptools.find_packages(),
   package_data={"spacy_ixakat":packages},
   install_requires=["spacy>=2.2.2","deplacy>=1.5.3"]
+  python_requires=">=3.6",
+  classifiers=[
+    "License :: OSI Approved :: MIT License",
+    "Programming Language :: Python :: 3",
+    "Operating System :: POSIX :: Linux",
+    "Topic :: Text Processing :: Linguistic",
+    "Natural Language :: Basque",
+  ],
+  project_urls={
+    "ixaKat":"http://ixa2.si.ehu.es/ixakat",
+    "Source":URL,
+    "Tracker":URL+"/issues",
+  }
 )
